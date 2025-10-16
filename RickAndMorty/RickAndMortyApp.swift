c8f4da53cbf79a77ce4e0f13c5016989b0687dc7
Bob Witmer <bwitmer@golden.net>
2025-10-16 10:06:50 -0400

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Bob Witmer on 2025-10-16.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
